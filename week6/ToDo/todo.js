f866fec5c68eb2c2e50cc70f6d0bb9ea0f7e5d4c
Lucy Davidson <dav18033@byui.edu>
2021-10-22 21:24:16 -0400

    fixing JS problems

--- week6/ToDo/todo.js
+++ week6/ToDo/todo.js
@@ -1,43 +1,40 @@
-const switcher = document.querySelector(' input[Type="checkbox"]');
+const checkedChecker = document.querySelector('input[Type="checkbox"]');
 const taskForm = document.querySelector(`[newTodoForm]`)
 const taskInput = document.querySelector(`[todoElements]`)
 
-const allTasksButton = document.getElementById('All')
-const activeTasksButton = document.getElementById('Active')
-const completedTasksButton = document.getElementById('Completed')
-
 let tasks = [];
 
+//listens for a submit in the html document for the newTodoForm and then takes the value of what was input in the todoElements tag
 taskForm.addEventListener('submit', e => {
     e.preventDefault()
     let task = taskInput.value.trim();
-    if(task !== '') {
-       addTask(task);
-       taskInput.value = '';
-       taskInput.focus();   //focuses the reloaded page on the input form
+    if(task !== '') {       //checks for input, if the user has actually given something, it can continue and  
+       addTask(task);       //add the task to the todo list
+       taskInput.value = '';  //clears the add task bar and 
+       taskInput.focus();   //refocuses the page on the bar again
     } 
 })
 
 // delete the item when the user clicks the X, otherwise, the item is struckthrough and marked completed
-document.getElementById('todo-list').addEventListener('click', e => {
+document.getElementById('todo-list').addEventListener('click', e => {   
   const itemKey = e.target.parentElement.dataset.key;
-  if (e.target.classList.contains('isChecked')) {
-        doneStaysDone(itemKey); 
+  if (e.target.classList.contains('isChecked')) {                       
+        doneStaysDone(itemKey);                                         
     }
-    if (e.target.classList.contains('delete')) {
+    if (e.target.classList.contains('delete')) {                         
         deleteTask(itemKey);
         counter();
       }    
 });
 
-// displays all the tasks on the list, complete or not
-allTasksButton.addEventListener('click',  (e) => {
+// displays all the tasks on the list, complete or not and retrieves items from the localStorage
+document.getElementById('All').addEventListener('click',  (e) => {
     document.getElementById('todo-list').innerHTML = ''
     const key = localStorage.getItem('tasksKey');
     if (key) {
       tasks = JSON.parse(key);
       tasks.forEach(task => {
-        renderTodo(task, true);
+        showTasks(task, true);
         counter();
       });
     }
@@ -45,45 +42,47 @@ allTasksButton.addEventListener('click',  (e) => {
 
 /*displays only the tasks that have not be completed, so 
   it removes the tasks that have not been checked */
-activeTasksButton.addEventListener('click',  (e) => {
+  document.getElementById('Active').addEventListener('click',  (e) => {
     document.getElementById('todo-list').innerHTML = ''
     const key = localStorage.getItem('tasksKey');
     if (key) {
       tasks = JSON.parse(key);
       tasks.filter(item => !item.checked).forEach(task => {
-        renderTodo(task, true);
+        showTasks(task, true);
         counter();
       });
     }
 })
 
 // displays only the tasks that have been marked as completed
-completedTasksButton.addEventListener('click',  (e) => {
+document.getElementById('Completed').addEventListener('click',  (e) => {
     document.getElementById('todo-list').innerHTML = ''
     const key = localStorage.getItem('tasksKey');
     if (key) {
       tasks = JSON.parse(key);
       tasks.filter(item=>item.checked).forEach(task => {
-        renderTodo(task, true);
+        showTasks(task, true);
         counter();
       });
     }
 })
 
-//this is the loacalStorage helper function that I decided to use
-//it keeps the tasks loaded so that a page refresh doesn't delete everything that's added
-document.addEventListener('DOMContentLoaded', () => {
+/****************************************Event Listener for the Local Storage*******************************************/
+
+//keeps the tasks loaded so that a page refresh doesn't delete everything that's added
+document.addEventListener('DOMContentLoaded', () => {     //when the content is loaded it pulls the previously existing tasks 
   const key = localStorage.getItem('tasksKey');
   if (key) {
-    //reads a value from local storage and parses it as a JSON
+    //reads a values in the array from local storage and parses it as a JSON
     tasks = JSON.parse(key);
     tasks.forEach(task => {
-      renderTodo(task);
+      showTasks(task);
       counter();
     });
   }
 });
 
+/****************************************All of the functions*******************************************/
 /* creates a todo object */
 const addTask = (text) => {
     const todoTask = {
@@ -92,11 +91,11 @@ const addTask = (text) => {
         id: Date.now(),
     }
     tasks.push(todoTask);
-    renderTodo(todoTask);
+    showTasks(todoTask);
 
 };
 
-const renderTodo = (todoTask, preventMutableStorage)=> {
+const showTasks = (todoTask, preventMutableStorage)=> {
   //stops data in the localstorage from being changed  
   if (!preventMutableStorage) {
         localStorage.setItem('tasksKey', JSON.stringify(tasks));
@@ -107,61 +106,64 @@ const renderTodo = (todoTask, preventMutableStorage)=> {
         return
       }
     const isChecked = todoTask.checked ? 'done': '';
-
     const node = document.createElement('li')
+    //assigns an added item to the list a class and assigns it to id=todo-item
     node.setAttribute('class', `todo-item ${isChecked}`);
     node.setAttribute('data-key', todoTask.id);
+    //add the html code for displaying each item with the newly assigned class, type, is, and checked status
     node.innerHTML = `
     <input class="isChecked" id="${todoTask.id}" type="checkbox" ${isChecked ? "checked" : ""}/>
     <span>${todoTask.text}</span>
     <span class="delete">X</span>`;
+    //adds new items onto the bottom of the list of tasks
     document.getElementById('todo-list').append(node);
-
     if (item) {
         node.replaceWith(item)
     } else {
-        document.getElementById('todo-list').append(node)
+        document.getElementById('todo-list').append(node);
     }
     counter();
 
+}
+
+//displays the completed tasks from the local storage, so past user's that left completed items on the list 
+// don't lose what they have completed
+const showCompletedTasks = () => {
+    const key = localStorage.getItem('tasksKey');
+    if (key) {
+      tasks = JSON.parse(key);
+      tasks.filter(item => item.check).forEach(task => {
+        showTasks(task, true);
+        counter();
+      });
+    }
 }
 
 //keeps the checked off items checked when the page changes or a button is clicked
 const doneStaysDone = (key) => {    
   const index = tasks.findIndex(task=> task.id === Number(key));
   tasks[index].checked = !tasks[index].checked;
-  renderTodo(tasks[index]);
-  renderCompletedTodos()
+  showTasks(tasks[index]);
+  showCompletedTasks()
 }
 
 //function to delete a task from the list whne the X is selected
 const deleteTask = (key) => {
   const index = tasks.findIndex(item => item.id === Number(key));
   const todoTask = {
-      ...tasks[index],
+      ...tasks[index],    //spreads the array into each element so they can be set to "deleted"
       deleted: true  
     };
     tasks = tasks.filter(item => item.id !== Number(key));
-  renderTodo(todoTask);
-
-}
-
-const renderCompletedTodos = () => {
-    const key = localStorage.getItem('tasksKey');
-    if (key) {
-      tasks = JSON.parse(key);
-      tasks.filter(item => item.check).forEach(task => {
-        renderTodo(task, true);
-        counter();
-      });
-    }
+  showTasks(todoTask);
+  
 }
 
 // to display how many items are left to be completed
 const counter = () => {
-    const itemsCounter =  tasks.filter(task=> !task.checked)
-    const count = document.getElementById('tasksLeft');
-    // has to check if there is more than one item in order to display plural items or not
-    const counterString = itemsCounter.length === 1 ? 'task' : 'tasks';
-    count.innerText = `${itemsCounter.length} ${counterString} left to do`
+  const itemsCounter =  tasks.filter(task=> !task.checked)
+  const count = document.getElementById('tasksLeft');
+  // has to check if there is more than one item in order to display plural items or not
+  const counterString = itemsCounter.length === 1 ? 'task' : 'tasks';
+  count.innerText = `${itemsCounter.length} ${counterString} left to do`
 }
