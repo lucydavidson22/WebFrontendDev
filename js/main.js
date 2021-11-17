2d587f13890ecc99143f6845c7599239deb9da0d
Lucy Davidson <dav18033@byui.edu>
2021-11-16 23:16:23 -0500

    adding labels

--- js/main.js
+++ js/main.js
@@ -34,5 +34,21 @@ const links = [
 {
   label: "Week9 notes",
   url: "week9/index9.html"
+},
+{
+  label: "Week10 notes",
+  url: "week10/index10.html"
+},
+{
+  label: "Week11 notes",
+  url: "week11/index11.html"
+},
+{
+  label: "Week12 notes",
+  url: "week12/index12.html"
+},
+{
+  label: "Week13 notes",
+  url: "week13/index13.html"
 }
   ]
