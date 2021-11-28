c59c7291d8641ab0a7202403eecfd806db981202
Lucy Davidson <dav18033@byui.edu>
2021-11-27 19:32:22 -0500

    adding the frontend for individuals and adding home page lists

--- Challenge2/christmas.js
+++ Challenge2/christmas.js
@@ -65,3 +65,112 @@ function clean0(timeto0) {
      document.getElementById("demo").innerHTML = "EXPIRED";
    }
  }
+
+
+
+
+//  ************************************Lists js ******************************
+///////////////////////////Mom List/////////////////////////////////////////////
+function addMomItem(){
+  var ul = document.getElementById("mom-list");
+  var momCandidate = document.getElementById("mom-candidate");
+  var li = document.createElement("li");
+  li.setAttribute('id',momCandidate.value);
+  li.appendChild(document.createTextNode(momCandidate.value));
+  ul.appendChild(li);
+  document.getElementById('mom-candidate').value=null;
+}
+
+function removeMomItem(){
+  var ul = document.getElementById("mom-list");
+  var momCandidate = document.getElementById("mom-candidate");
+  var item = document.getElementById(momCandidate.value);
+  ul.removeChild(item);
+}
+///////////////////////////Dad List/////////////////////////////////////////////
+function addDadItem(){
+  var ul = document.getElementById("dad-list");
+  var dadCandidate2 = document.getElementById("dad-candidate");
+  var li = document.createElement("li");
+  li.setAttribute('id',dadCandidate2.value);
+  li.appendChild(document.createTextNode(dadCandidate2.value));
+  ul.appendChild(li);
+  document.getElementById('dad-candidate').value=null;
+}
+
+function removeDadItem(){
+  var ul = document.getElementById("dad-list");
+  var dadCandidate2 = document.getElementById("dad-candidate");
+  var item = document.getElementById(dadCandidate2.value);
+  ul.removeChild(item);
+}
+///////////////////////////Siblings List/////////////////////////////////////////////
+function addSiblingsItem(){
+  var ul = document.getElementById("siblings-list");
+  var siblingsCandidate = document.getElementById("siblings-candidate");
+  var li = document.createElement("li");
+  li.setAttribute('id',siblingsCandidate.value);
+  li.appendChild(document.createTextNode(siblingsCandidate.value));
+  ul.appendChild(li);
+  document.getElementById('siblings-candidate').value=null;
+}
+
+function removeSiblingsItem(){
+  var ul = document.getElementById("siblings-list");
+  var siblingsCandidate = document.getElementById("siblings-candidate");
+  var item = document.getElementById(siblingsCandidate.value);
+  ul.removeChild(item);
+}
+///////////////////////////Friends List/////////////////////////////////////////////
+function addFriendsItem(){
+  var ul = document.getElementById("friends-list");
+  var friendsCandidate = document.getElementById("friends-candidate");
+  var li = document.createElement("li");
+  li.setAttribute('id',friendsCandidate.value);
+  li.appendChild(document.createTextNode(friendsCandidate.value));
+  ul.appendChild(li);
+  document.getElementById('friends-candidate').value=null;
+}
+
+function removeFriendsItem(){
+  var ul = document.getElementById("friends-list");
+  var friendsCandidate = document.getElementById("friends-candidate");
+  var item = document.getElementById(friendsCandidate.value);
+  ul.removeChild(item);
+}
+
+///////////////////////////Pets List/////////////////////////////////////////////
+function addPetsItem(){
+  var ul = document.getElementById("pets-list");
+  var petsCandidate = document.getElementById("pets-candidate");
+  var li = document.createElement("li");
+  li.setAttribute('id',petsCandidate.value);
+  li.appendChild(document.createTextNode(petsCandidate.value));
+  ul.appendChild(li);
+  document.getElementById('pets-candidate').value=null;
+}
+
+function removePetsItem(){
+  var ul = document.getElementById("pets-list");
+  var petsCandidate = document.getElementById("pets-candidate");
+  var item = document.getElementById(petsCandidate.value);
+  ul.removeChild(item);
+}
+
+///////////////////////////Others List/////////////////////////////////////////////
+function addOthersItem(){
+  var ul = document.getElementById("others-list");
+  var othersCandidate = document.getElementById("others-candidate");
+  var li = document.createElement("li");
+  li.setAttribute('id',othersCandidate.value);
+  li.appendChild(document.createTextNode(othersCandidate.value));
+  ul.appendChild(li);
+  document.getElementById('others-candidate').value=null;
+}
+
+function removeOthersItem(){
+  var ul = document.getElementById("others-list");
+  var othersCandidate = document.getElementById("others-candidate");
+  var item = document.getElementById(othersCandidate.value);
+  ul.removeChild(item);
+}
